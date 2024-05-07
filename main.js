ae4d0b98bc7499c3a9ab870d29520faa5b1b7295
Chris Nelson <chrisnelson7791@gmail.com>
2024-05-07 16:50:25 -0700

    First commit. .length property

---
+++ main.js
@@ -0,0 +1,12 @@
+// ~~~~~ String Methods and Properties! ~~~~~
+
+// .length
+// note that this is a property NOT a method!
+// explanation: returns the number of characters in a string
+// use: compare the size of multiple strings 
+
+const str = 'This town aint big enough for the two of us'
+console.log(str.length)
+
+
+
