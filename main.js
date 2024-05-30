c8e259a955187f96192bfea0cc2aa1ad462be567
Chris Nelson <chrisnelson7791@gmail.com>
2024-05-29 19:23:58 -0700

    required and optional parameters added for each string method so far

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // .length
 // note that this is a property NOT a method!
 // explanation: returns the number of characters in a string
+// str.length   // no parameters because it's a property!
 // use: compare the size of multiple strings 
 
 const str = 'This town aint big enough for the two of us'
@@ -13,6 +14,7 @@ console.log(str.length)
 // .trim()
 // explanation: removes white space from the beginning and end of a string
 // does NOT mutate the original string
+// str.trim()   // no parameters
 // use: processing user input fields
 
 const str1 = '     oh hey Jimmy  '
@@ -24,6 +26,8 @@ console.log('str1: ', str1.trim().length)
 
 // .includes()
 // explanation: returns a true or false if the called upon string contains the specified substring
+// str.includes(searchString)
+// str.includes(searchString, position)     // position: the index within the string at which to begin the search
 // use: check if a string contains a substring
 
 const str2 = 'Stone Cold Steve Austin'
@@ -39,6 +43,8 @@ console.log( str4.includes('Steve') )
 // .indexOf()
 // explanation: returns the index of the specified substring if its contained within the called upon string
 // returns -1 if the substring does not exist within the string
+// str.indexOf(searchString)
+// str.indexOf(searchString, position)    // position: specifies an index at which to begin the search. searchString may be found at an index equal to or greater than the specified position
 // use: check if  a string contains a substring
 
 console.log( str2.indexOf('Steve') )
@@ -49,6 +55,7 @@ console.log( str4.indexOf('Steve') )
 
 // .toUpperCase()
 // explanation: returns a string with all letters capitalized
+// str.toUpperCase()    // no parameters
 // use: eliminate case typos before evaluating string with another method
 
 str5 = 'Hello there, General Kenobi'
@@ -59,6 +66,7 @@ console.log( str5.toUpperCase() )
 
 // .toLowerCase()
 // explanation: returns a string with all letters lower case
+// str.toLowerCase()    // no parameters
 // use: eliminate case typos before evaluating string with another method
 
 console.log( str5.toLowerCase() )
@@ -67,6 +75,7 @@ console.log( str5.toLowerCase() )
 
 // .replace()
 // explanation: replaces the first occurence of a pattern with the specified replacement
+// str.replace(pattern, replacement)
 // use: change a user's name in a block of text that will be displayed to individual viewers
 
 str6 = 'Harry Potter and the blank'
@@ -80,6 +89,7 @@ console.log( str6.replace('blank', strReplacement))
 // explanation: grabs a section of a string starting with the supplied index until the last index, or until the indexEnd if specified
 // str.slice(indexStart)
 // str.slice(indexStart, indexEnd)
+// use: extract a substring of a name after a known punctuation mark in a string
 
 let str7 = 'Hello, this is my name: George Q Georgeson'
 let lastName = str7.slice(24)
@@ -89,3 +99,6 @@ let fullName = str7.slice(str7.indexOf(':') + 2)
 console.log(lastName)
 console.log(firstName)
 console.log(fullName)
+
+
+
